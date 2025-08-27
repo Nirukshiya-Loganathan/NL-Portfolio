d0b6a7bf29631e9a81e24c99ea63b66a4623edb4
Nirukshiya-Loganathan <kslniru11@gmail.com>
2025-08-27 21:42:52 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
-  //base: '/NL-Portfolio/' 
+  //base: '/NL-Portfolio/'   
+  "deploy:full": "npm run build && npm run deploy"
 })
