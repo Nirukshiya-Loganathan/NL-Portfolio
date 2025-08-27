5fe1dfb09e65f0a596a9e219268a11640cfdd73e
Nirukshiya-Loganathan <kslniru11@gmail.com>
2025-08-27 22:54:51 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/NL-Portfolio/"> {/* <-- add basename */}
       <App />
     </BrowserRouter>
   </React.StrictMode>
